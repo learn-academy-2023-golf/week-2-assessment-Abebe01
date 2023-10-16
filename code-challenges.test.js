4e3015574abb93afbb6f771fff1396ff9135b64a
Asrat <aabebe.navy@gmail.com>
2023-10-16 08:32:20 -0700

    week 2 asses complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -33,19 +33,96 @@ const object2 = { number: 0 }
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
+describe('divideByThree', () => {
+    it('takes an object as an argument and decides if the number inside it is evenly divisible by three or not.', () => {
+      const object1 = { number: 15 }
+      // Expected output: "15 is divisible by three"
+      const object2 = { number: 0 }
+      // Expected output: "0 is divisible by three"
+      const object3 = { number: -7 }
+      // Expected output: "-7 is not divisible by three"
+      expect(divideByThree(object1.number)).toEqual("15 is divisible by three")
+      expect(divideByThree(object2.number)).toEqual("0 is divisible by three")
+      expect(divideByThree(object3.number)).toEqual("-7 is not divisible by three")
+    })
+  })
+// ReferenceError: divideByThree is not defined
+
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// Pseudo code: 
+// input: object
+// output: object with it's potential to be divided by 3.
+
+// const divideByThree = (object1, object2, object3) => {
+//     if (object1 % 3 === 0) {
+//     return ["15 is divisible by three"]
+//     } else if (object2 % 3 === 0) {
+//         return ["0 is divisible by three"]
+// } else if (object3 % 3 !== 0) {
+//     return "-7 is not divisible by three"
+// }
+// }
+
+const divideByThree = (num) => {
+if (num % 3 === 0) {
+    return `${num} is divisible by three`
+} else {
+    return `${num} is not divisible by three`
+}
+}
+
+
+    
+//     (num => {
+//         return num % 3 === 0
+//     })
+// }
+
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+describe('allCaps', () => {
+    it('takes in an array of words and returns an array with all the words capitalized.', () => {
+    const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+    // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+    const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+    // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+      expect(allCaps(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+      expect(allCaps(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+    })
+  })
+//   ReferenceError: allCaps is not defined
+
+
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+const allCaps = (arr) => {
+    return arr.map((value) => {
+        return value[0].toUpperCase() + value.substring(1)
+    })
+  }
+// Pseudo code: 
+// create a function that takes in an array of words and returns array with all the words capitalized
+// declare a function that takes in an array
+// return the array output from the map action
+// with each value access the first index and capitalize the letter then concatenate the remainder of the word
+// logging the function and pass in the array
+// output : an array with the first letter of each word capitalized.
+
+
+// //  PASS  ./code-challenges.test.js
+//   divideByThree
+//   ✓ takes an object as an argument and decides if the number inside it is evenly divisible by three or not. (2 ms)
+// allCaps
+//   ✓ takes in an array of words and returns an array with all the words capitalized. (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.363 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 1.19s.
--- code-critique.js
+++ code-critique.js
@@ -6,5 +6,55 @@
 
 // Copy and paste the code output here.
 
+// def find_first_vowel_index(input_string):
+    // # Define a list of vowels
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    // # Convert the input string to lowercase to handle both lowercase and uppercase vowels
+    input_string = input_string.lower()
+
+    // # Loop through each character in the string
+    // for index, char in enumerate(input_string):
+        // if char in vowels:
+            // # Log the index of the first vowel and return it
+            // print(f"The first vowel '{char}' is at index {index}.")
+            return index
+
+    // # If no vowel is found, log a message and return -1
+    // print("No vowels found in the input string.")
+    return -1
+
+// # Example usage:
+input_str = "Hello, World!"
+first_vowel_index = find_first_vowel_index(input_str)
+
+
+
+
+
+
+
 // Critique the code output. 
 // Things to consider in your critique: Were you surprised by the output? Was the output what you expected? Is the correct output being rendered? What do you like about the code? What do you dislike? Are there best practices that are not being followed? Would you take a different approach? What did you learn? 
+// Were you surprised by the output? 
+
+
+// Was the output what you expected? - No! I thought the code returned would be a lot more concise and put together. But for some reason it did not even it essentially formed an array of "string" vowels, and repeated the prompt while breaking down the already incorrect code provided. It's use of pseudo code did surprise me however!
+
+
+// Is the correct output being rendered? - No, not at all. 
+
+
+// What do you like about the code? - The psuedo code for sure. I think it's pretty cool that the idea of showing your work or explaining it is so common place in coding that even if the Ai didn't return exactly what was desired. It understood that it should explain it's method of thinking. 
+
+
+// What do you dislike? - It's misiterpertation of the prompt. Or, I guess how it broke the prompt down into unrelated subtasks or prompts.
+
+
+// Are there best practices that are not being followed?  - I feel as thouhh I may be too inexperienced or uneducated to answer this question; but to my knowledge the best practices are technically being followed (again really just emphasizing the pseudo code here).
+
+
+//  Would you take a different approach? What did you learn? - Yes ! I would take a differen't approach. I would actually follow the prompt haha. But I did learn, while Ai continues to be a great friend for developers and programmers, we cannot completley rely on it to follow and execute our exact wants and needs. 
+
+// ^ How I'd do the prompt: 
+
